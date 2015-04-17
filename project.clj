ecd7c95817d9332898d080148700cc1ea50928fd
wgb <wbecker@roomkey.com>
2015-04-17 16:26:03 -0400

    Version 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.promotably/apollo "0.2.3"
+(defproject org.clojars.promotably/apollo "0.2.4"
   :description "A clojure library for Amazon Cloudwatch"
   :url "http://github.com/promotably/apollo"
   :license {:name "Eclipse Public License"
